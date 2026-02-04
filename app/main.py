19de114fb63273cfbbcb1b21145d49e391791619
Михаил Брагин <mbragins@mail.ru>
2026-02-04 09:19:42 +0500

    commit1

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from app.api import router
+from app.database import engine
+from app.models import Base
 import uvicorn
 from dotenv import load_dotenv
 
 # Загружаем переменные окружения из .env файла
 load_dotenv()
 
+# Создание FastAPI приложения
 app = FastAPI(
-    title="Order Service", description="Сервис для управления заказами", version="1.0.0"
+    title="Order Service",
+    description="Сервис для управления заказами",
+    version="1.0.0"
 )
 
-# Подключаем роутер
+# Подключаем роутер&
 app.include_router(router, prefix="/api")
-
 
 # Корневой эндпоинт для проверки
 @app.get("/")
 async def root():
-    return {"message": "Order Service is running"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    return {"message": "Order Service запущен"}
