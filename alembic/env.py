ecc6f364ac01770b6396178bc6771f40df4bab1f
Михаил Брагин <mbragins@mail.ru>
2026-02-11 15:01:02 +0500

    correct_env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,21 +1,16 @@
 import os
 import sys
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
+from sqlalchemy import create_engine
 from alembic import context
-
 # Добавляем путь к проекту
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# 1. Сначала модели
 from app.models import *
-
-# 2. Потом Base
 from app.database_sync import Base
-from dotenv import load_dotenv
+
+
+def get_url():
+    return os.getenv("DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -50,7 +45,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -69,11 +64,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(get_url())
 
     with connectable.connect() as connection:
         context.configure(
