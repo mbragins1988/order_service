0cdd52e4fd87c5e6da6942936c5ddeead6c24a67
Михаил Брагин <mbragins@mail.ru>
2026-02-14 00:44:00 +0500

    check_value

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 export PYTHONUNBUFFERED=1 
 echo "Applying database migrations..."
+echo "POSTGRES_CONNECTION_STRING: $POSTGRES_CONNECTION_STRING"
+echo "POSTGRES_DATABASE_NAME: $POSTGRES_DATABASE_NAME"
+echo "POSTGRES_HOST: $POSTGRES_HOST"
+echo "POSTGRES_PORT: $POSTGRES_PORT"
+echo "POSTGRES_USERNAME: $POSTGRES_USERNAME"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo "==================================="
 alembic upgrade head
 
 # echo "Starting workers..."
