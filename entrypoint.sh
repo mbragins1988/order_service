ff478dc0a894af4a79136664e979a2640b8a97e9
Михаил Брагин <mbragins@mail.ru>
2026-02-14 18:26:57 +0500

    workers_added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ echo "Applying database migrations"
 alembic upgrade head
 
 # echo "Starting workers..."
-# python -m app.outbox_worker &
-# python -m app.inbox_worker &
-# python -m app.shipping_worker &
+python -m app.outbox_worker &
+python -m app.inbox_worker &
+python -m app.shipping_worker &
 
 echo "Starting FastAPI application..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
